01e594ef4c6a302715f2d298822f0101efc9877e
Elliot Nash <elliotnash@gmail.com>
2026-01-14 21:59:15 -0800

    feat: Add default adwaita theme colours as fallback

--- src/gtk/current.rs
+++ src/gtk/current.rs
@@ -3,7 +3,11 @@ use std::path::PathBuf;
 
 use cssparser::Color;
 
-use super::parse::{from_file, DefineColor};
+use super::parse::{from_file, from_str, DefineColor};
+
+const PALETTE_CSS: &str = include_str!("css/palette.css");
+const LIGHT_CSS: &str = include_str!("css/light.css");
+const DARK_CSS: &str = include_str!("css/dark.css");
 
 #[cfg(all(unix, not(target_os = "macos")))]
 /**
@@ -13,6 +17,8 @@ use super::parse::{from_file, DefineColor};
  */
 pub fn current() -> (HashMap<String, Color>, Vec<Error>) {
     use std::env;
+
+    
 
     let (mut map, mut error) = if let Ok(name) = std::env::var("GTK_THEME") {
         get_theme(&name)
@@ -41,22 +47,20 @@ pub fn get_theme(name: &str) -> (HashMap<String, Color>, Vec<Error>) {
     (themes, errors)
 }
 
-fn push(theme: &PathBuf, themes: &mut HashMap<String, Color>, errors: &mut Vec<Error>) {
-    if theme.exists() {
-        match from_file(theme) {
-            Ok(theme) => {
-                for DefineColor {
-                    ident,
-                    color,
-                    loc: _,
-                } in theme
-                {
-                    themes.insert(ident, color);
-                }
+fn push(theme: Result<Vec<DefineColor>, crate::gtk::parse::Error>, themes: &mut HashMap<String, Color>, errors: &mut Vec<Error>) {
+    match theme {
+        Ok(theme) => {
+            for DefineColor {
+                ident,
+                color,
+                loc: _,
+            } in theme
+            {
+                themes.insert(ident, color);
             }
-            Err(error) => errors.push(Error::ParsingError(error)),
-        };
-    }
+        }
+        Err(error) => errors.push(Error::ParsingError(error)),
+    };
 }
 
 fn get_theme_on_folder(
@@ -64,18 +68,28 @@ fn get_theme_on_folder(
     themes: &mut HashMap<String, Color>,
     errors: &mut Vec<Error>,
 ) {
+    let mode = dark_light::detect();
+
+    let default_css = PALETTE_CSS.to_owned() + if mode == dark_light::Mode::Dark {
+        DARK_CSS
+    } else {
+        LIGHT_CSS
+    };
+
+    push(from_str(&default_css), themes, errors);
+
     if path.exists() {
         let normal = path.join("gtk.css");
 
         if normal.exists() {
-            push(&normal, themes, errors);
+            push(from_file(&normal), themes, errors);
         }
 
-        if dark_light::detect() == dark_light::Mode::Dark {
+        if mode == dark_light::Mode::Dark {
             let dark = PathBuf::from(path).join("gtk-dark.css");
 
             if dark.exists() {
-                push(&dark, themes, errors);
+                push(from_file(&dark), themes, errors);
             }
         }
     }
